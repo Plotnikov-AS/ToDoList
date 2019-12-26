e9c57df1b471170a0631397f7bc725b3ecb699a8
Plotnikov-AS <plotnikovatnonserg@gmail.com>
2019-12-26 21:29:31 +0300

    init commit

---
+++ src/main/java/main/TestController.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class TestController {
+    @RequestMapping("/")
+    public String index(){
+        return (new Date().toString());
+    }
+}
